aa90b5db0e06433c0b374ae98f97cc0908df3f73
Barriej63 <barriejennings2@gmail.com>
2025-08-20 14:28:58 +1200

    Delete page.js

--- page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{display:'flex',height:'100vh',justifyContent:'center',alignItems:'center'}}>
-      <h1>✅ Good2Go Staging Placeholder</h1>
-    </main>
-  )
-}
