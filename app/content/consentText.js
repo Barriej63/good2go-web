aba4f02713dd08910837344623de4e397a8f62e9
Barriej63 <barriejennings2@gmail.com>
2025-08-23 18:03:36 +1200

    Add files via upload

---
+++ app/content/consentText.js
@@ -0,0 +1,7 @@
+export const CONSENT_VERSION = "2025-08-23";
+
+export const CONSENT_SUMMARY = [
+  "I consent to Good2Go sharing relevant assessment results with my nominated referring medical professional for the purpose of ongoing care.",
+  "I understand I can revoke consent at any time in writing, except where action has already been taken based on this consent.",
+  "I acknowledge Good2Go is a clinical decision support (CDS) tool, not a diagnostic instrument."
+];
